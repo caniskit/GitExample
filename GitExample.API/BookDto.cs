bec9fe0b33693e977fa1522901ccc958a4748ee8
Fatih Çakıroğlu <fatih.cakiroglu.dn@aykargo.com.tr>
2022-04-17 15:30:36 +0300

    price2 oldu

--- GitExample.API/BookDto.cs
+++ GitExample.API/BookDto.cs
@@ -3,7 +3,7 @@
     public class BookDto
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public decimal Price2 { get; set; }
         public decimal Price { get; set; }
     }
 }
